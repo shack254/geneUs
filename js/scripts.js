fcba661c24ac1fba27379595cbe3fd38e17a5e6e
shack254 <123meshack@gmail.com>
2019-06-21 19:22:59 +0300

    created a construct for popup charts

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@ var DNA = "";
 var sortedObj = [];
 var topCodons = [];
 var chartMaker = {};
+
 function breakMaps(DNA) {
     var newList = [];
     var maplength = 3;
@@ -31,28 +32,30 @@ function breakMaps(DNA) {
     }
     return createCounter();
 
-} 
-function sort(){
+}
+
+function sort() {
     breakMaps(DNA)
-    for (key in dnaCounter){ 
+    for (key in dnaCounter) {
         sortedObj.push([key, dnaCounter[key]]);
     }
-    sortedObj.sort(function(a, b) {
+    sortedObj.sort(function (a, b) {
         return b[1] - a[1];
     });
-    function topTen(){
-        sortedObj.forEach(function(codon){  
-     if(sortedObj.indexOf(codon) < 10){ 
-         topCodons.push(codon)
-     }
+
+    function topTen() {
+        sortedObj.forEach(function (codon) {
+            if (sortedObj.indexOf(codon) < 10) {
+                topCodons.push(codon)
+            }
         })
-        topCodons.forEach(function(codon){
+        topCodons.forEach(function (codon) {
             chartMaker[codon[0]] = codon[1]
-            })
-            alert(chartMaker)
-            return (chartMaker)
-     }
-   return (topTen()) 
+        })
+        alert(chartMaker)
+        return (chartMaker)
+    }
+    return (topTen())
 
 }
 
@@ -60,7 +63,7 @@ function sort(){
 function sickleCell() {
     breakMaps(DNA);
     var map = "GAG";
-    for ( var key in dnaCounter) {
+    for (var key in dnaCounter) {
         if (key === map) {
             var total = (DNAlist.length);
             var proportion = parseFloat((dnaCounter[key]) / total)
@@ -72,6 +75,7 @@ function sickleCell() {
     }
     return (result)
 }
+
 function cystic() {
     breakMaps(DNA);
     var map = "TTT";
@@ -82,120 +86,122 @@ function cystic() {
             var percentage = proportion * 100;
             var result = Math.round(percentage);
             sort()
-            alert(result+ "%");
+            alert(result + "%");
         }
-       
+
     }
     return (result);
 }
-    function fragilo() {
-        breakMaps(DNA);
-        var map = "CGG";
-        for (var key in dnaCounter) {
-            if (key === map) {
-               var total = (DNAlist.length);
-                var proportion = parseFloat((dnaCounter[key]) / total);
-                var percentage = proportion * 100;
-                var result = Math.round(percentage);
-                sort()
-                alert(result + "%");
-            }
-        }
-        return (result);
-    }
-    function tay() {
-        breakMaps(DNA);
-        var map = "CAA";
-        for (var key in dnaCounter) {
-            if (key === map) {
-               var total = (DNAlist.length);
-                var proportion = parseFloat((dnaCounter[key]) / total);
-                var percentage = proportion * 100;
-                var result = Math.round(percentage);
-                sort()
-                alert(result+ "%");
-            }
-        }
-        return (result);
 
+function fragilo() {
+    breakMaps(DNA);
+    var map = "CGG";
+    for (var key in dnaCounter) {
+        if (key === map) {
+            var total = (DNAlist.length);
+            var proportion = parseFloat((dnaCounter[key]) / total);
+            var percentage = proportion * 100;
+            var result = Math.round(percentage);
+            sort()
+            alert(result + "%");
+        }
     }
-    function Chart(name ,lables,datasets,data ,options,legend ,layout){
-        this.name = name;
-        this.lables = lables;
-        this.datasets = datasets;
-        this.data = data ;
-        this.options = options;
-        this.legend = legend;
-        this.layout = layout;
-    }
+    return (result);
+}
 
-    function chartUp(){
-        topCodons.forEach(function(codon){
-            popChart.data.labels.push(codon[0])
-            popChart.data.datasets[0].data.push(codon[1])
-        })
-        var myChart = $(".myChart")
-        var popChart = new Chart (myChart,{ 
-            type: "doughnut",
-            data : { 
-        labels : [] ,
-        datasets : [{label : "Top 10 Codons",
-        data : []
-        }]
+function tay() {
+    breakMaps(DNA);
+    var map = "CAA";
+    for (var key in dnaCounter) {
+        if (key === map) {
+            var total = (DNAlist.length);
+            var proportion = parseFloat((dnaCounter[key]) / total);
+            var percentage = proportion * 100;
+            var result = Math.round(percentage);
+            sort()
+            alert(result + "%");
+        }
+    }
+    return (result);
+
+}
+
+function chartUp() {
+    var mapped_data = topCodons.map(function (ele) {
+        return {
+            x: ele[0],
+            y: ele[1]
+        }
+    });
+    topCodons.forEach(function (codon) {
+        popChart.data.labels.push(codon[0])
+        popChart.data.datasets[0].data.push(codon[1])
+    })
+    var myChart = $(".myChart")
+    var popChart = new Chart(myChart, {
+        type: "doughnut",
+        data: {
+            labels: [],
+            datasets: [{
+                label: "Top 10 Codons",
+                data: [mapped_data]
+            }]
         },
         options: {
-        title: { 
-        display: true,
-        text: "Top 10 Codons",
-        fontSize: 25
-        },
-        legend:{
-        display : true,
-        position : "right",
-        labels : { 
-        fontColor : 000
-        },
-        layout :{
-        padding : {
-        left: 50
-        }
-        }
-    }
+            title: {
+                display: true,
+                text: "Top 10 Codons",
+                fontSize: 25
+            },
+            legend: {
+                display: true,
+                position: "right",
+                labels: {
+                    fontColor: 000
+                },
+                layout: {
+                    padding: {
+                        left: 50
+                    }
+                }
+            }
         }
     })
-
-    }
-
-    $(document).ready(function () {
-        $(".first-disease").hide();
-        $(".second-disease").hide();
-        $(".third-disease").hide();
-        $(".fourth-disease").hide();
-        $("#file").submit(function (event) {
-            event.preventDefault();
-            DNA = DNA + document.getElementById("sequence").value;
-            alert(DNA);
-            chartUp()
-            return (DNA);
-        }
-        )
-        $("#sickleCell").click(function (event) {
-            event.preventDefault
-            sickleCell()
-        })
-        $("#cystic").click(function (event) {
-            event.preventFunction
-            cystic()
-        })
-
-        $("#fragilo").click(function (event) {
-            event.preventDefault
-            fragilo()
-        })
-
-        $("#tay").click(function (event) {
-            event.preventDefault
-            tay()
-        })
-
+}
+var myDoughnutChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: topCodons,
+    options: options
+});
+$(document).ready(function () {
+    $(".first-disease").hide();
+    $(".second-disease").hide();
+    $(".third-disease").hide();
+    $(".fourth-disease").hide();
+    $("#file").submit(function (event) {
+        event.preventDefault();
+        DNA = DNA + document.getElementById("sequence").value;
+        alert(DNA);
+        sort()
+        return (DNA);
     })
+    $("#sickleCell").click(function (event) {
+        event.preventDefault
+        sickleCell()
+    })
+    $("#cystic").click(function (event) {
+        event.preventFunction
+        cystic()
+    })
+
+    $("#fragilo").click(function (event) {
+        event.preventDefault
+        fragilo()
+    })
+
+    $("#tay").click(function (event) {
+        event.preventDefault
+        tay()
+    })
+
+})
